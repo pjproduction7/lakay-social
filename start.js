14ebf931cdfed9bb2beae8d9bd1899b8c505b278
James Prosper <pjproduction7@gmail.com>
2026-02-17 09:06:02 -0600

    realtime: add upgrade/handshake logging; client: polling fallback for railway

---
+++ start.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// Entry point used by some hosting platforms that run `node start.js`.
+// This simply delegates to the server's actual entrypoint.
+import './server/src/index.js';
