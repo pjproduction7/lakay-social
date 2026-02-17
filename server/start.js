59eaad3f15e8583210cf400b462805ce70e55e5d
James Prosper <pjproduction7@gmail.com>
2026-02-17 17:21:47 -0600

    chore: add server/start.js for Railway start command

---
+++ server/start.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// Hosting platforms that call `node start.js` from the `server/` working directory
+// will find this file and delegate to the real entrypoint.
+import './src/index.js';
